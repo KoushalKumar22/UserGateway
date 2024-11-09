73ff2469099481e888aa08b981876d49a3c4867e
KoushalKumar22 <koushalkumar2224@gmail.com>
2024-11-09 22:24:33 +0530

    Deployment

---
+++ src/main/java/com/UserAcces/UsersGateway/UsersGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.UserAcces.UsersGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/UserAcces/UsersGateway/UsersGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.UserAcces.UsersGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
